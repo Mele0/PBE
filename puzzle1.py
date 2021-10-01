9b4f38d0101844c799c0d69af9a683264e15a250
Alex <90758727+Mele0@users.noreply.github.com>
2021-10-01 20:52:26 +0200

    Update puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
-import time
 from mfrc522 import SimpleMFRC522
 
 GPIO.setwarnings(False)
@@ -12,13 +11,13 @@ class Rfid_rc522:
         return uid[0]
     
 if __name__ == "__main__":
-        print('Please insert a student card')
-        while True:
-            try:    
-                    rf = Rfid_rc522()
-                    uid = rf.scan_uid()
-                    print(hex(uid).upper())
-                    time.sleep(2)
-            finally:
-                    GPIO.cleanup()
+    
+        print('Please insert your card')
+        try:    
+               rf = Rfid_rc522()
+               uid = rf.scan_uid()
+               print(hex(uid).upper())
+                
+        finally:
+               GPIO.cleanup()
                 
